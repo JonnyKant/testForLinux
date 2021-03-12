e2f829fa9afbb4b8c27b6292c13589e86bd03962
lion_raiden <dionraide@gmail.com>
2021-03-12 21:37:53 +0300

    1.4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         File file = new File("new.txt");
         FileWriter fileWriter = new FileWriter(file);
-        String s = new String("0 1 2 3 4 5" + "\n");
+        String s = new String("0 1 2 3 4 5" + System.lineSeparator());
         fileWriter.write(s);
         fileWriter.close();
         FileInputStream fileInputStream = new FileInputStream(file);
         byte[] d = fileInputStream.readAllBytes();
         for(byte a : d) {
+            System.out.printf("%02x",a);
+            System.out.println("");
             System.out.println(a);
         }
     }
